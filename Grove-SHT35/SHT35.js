76c1ebea44ee826a94b66687ac44f2c45f27019a
Tanaka Seigo <seigo@1ft-seabass.jp>
2021-01-21 10:22:41 +0900

    const 変数化する

--- Grove-SHT35/SHT35.js
+++ Grove-SHT35/SHT35.js
@@ -1,63 +1,59 @@
-// Seeed_SHT35.cpp の定数を持ってくる
-
+// この enum は変数指定と変わらない
+/*
 typedef enum {
   NO_ERROR = 0,
   ERROR_PARAM = -1,
   ERROR_COMM = -2,
   ERROR_OTHERS = -128,
 } err_t;
+*/
+
+const NO_ERROR = 0;
+const ERROR_PARAM = -1;
+const ERROR_COMM = -2;
+const ERROR_OTHERS = -128;
+
+// VSCode で next occurrence Ctrl+D を多用して空白を選択し一括編集していく
+
+const DEFAULT_IIC_ADDR = 0x45;
+const NACK_ON_ADDR = 2;
 
 
-#define CHECK_RESULT(a,b)   do{if(a=b)  {    \
-          SERIAL_DB.print(__FILE__);    \
-          SERIAL_DB.print(__LINE__);   \
-          SERIAL_DB.print(" error code =");  \
-          SERIAL_DB.println(a);                   \
-          return a;   \
-      }}while(0)
+const CLK_STRETCH_ENABLED = 0;
+const CLK_STRETCH_DISABLED = 3;
 
-#endif
+const MODE_MPS_05 = 6;
+const MODE_MPS_1 = 9;
+const MODE_MPS_2 = 12;
+const MODE_MPS_4 = 15;
+const MODE_MPS_10 = 18;
 
+const REPEAT_HIGH = 0;
+const REPEAT_MED = 1;
+const REPEAT_LOW = 2;
 
-#define     DEFAULT_IIC_ADDR     0x45
-#define		NACK_ON_ADDR			2
+const CMD_BREAK = 0x3093;
+const CMD_SOFT_RST = 0x30A2;
+const CMD_ENABLE_HEAT = 0x306D;
+const CMD_DISABLE_HEAT = 0x3066;
+const CMD_READ_SREG = 0xF32D;
+const CMD_CLEAR_SREG = 0x3041;
+const CMD_FETCH_DATA = 0xE000;
 
+const CMD_READ_HIGH_ALERT_LIMIT_SET_VALUE = 0XE11F;
+const CMD_READ_HIGH_ALERT_LIMIT_CLEAR_VALUE =0XE114;
+const CMD_READ_LOW_ALERT_LIMIT_SET_VALUE =  0XE102;
+const CMD_READ_LOW_ALERT_LIMIT_CLEAR_VALUE = 0XE109;
 
-#define		CLK_STRETCH_ENABLED		0
-#define		CLK_STRETCH_DISABLED	3
+const CMD_WRITE_HIGH_ALERT_LIMIT_SET_VALUE = 0X611D;
+const CMD_WRITE_HIGH_ALERT_LIMIT_CLEAR_VALUE = 0X6116;
+const CMD_WRITE_LOW_ALERT_LIMIT_SET_VALUE = 0X6100;
+const CMD_WRITE_LOW_ALERT_LIMIT_CLEAR_VALUE = 0X610B;
 
-#define		MODE_MPS_05				6
-#define		MODE_MPS_1				9
-#define		MODE_MPS_2				12
-#define		MODE_MPS_4				15
-#define		MODE_MPS_10				18
+const HIGH_REP_WITH_STRCH = 0x2C06;
 
-#define		REPEAT_HIGH				0
-#define		REPEAT_MED				1
-#define		REPEAT_LOW				2
-
-#define		CMD_BREAK			0x3093
-#define		CMD_SOFT_RST		0x30A2
-#define		CMD_ENABLE_HEAT		0x306D
-#define		CMD_DISABLE_HEAT	0x3066
-#define		CMD_READ_SREG		0xF32D
-#define		CMD_CLEAR_SREG		0x3041
-#define		CMD_FETCH_DATA		0xE000
-
-#define     CMD_READ_HIGH_ALERT_LIMIT_SET_VALUE     0XE11F
-#define     CMD_READ_HIGH_ALERT_LIMIT_CLEAR_VALUE   0XE114
-#define     CMD_READ_LOW_ALERT_LIMIT_SET_VALUE      0XE102
-#define     CMD_READ_LOW_ALERT_LIMIT_CLEAR_VALUE    0XE109
-
-#define     CMD_WRITE_HIGH_ALERT_LIMIT_SET_VALUE    0X611D
-#define     CMD_WRITE_HIGH_ALERT_LIMIT_CLEAR_VALUE  0X6116
-#define     CMD_WRITE_LOW_ALERT_LIMIT_SET_VALUE     0X6100
-#define     CMD_WRITE_LOW_ALERT_LIMIT_CLEAR_VALUE     0X610B
-
-#define     HIGH_REP_WITH_STRCH      0x2C06
-
-#define     CMD_HEATER_ON     0x306D
-#define     CMD_HEATER_OFF    0x3066
+const CMD_HEATER_ON = 0x306D;
+const CMD_HEATER_OFF = 0x3066;
 
 var Obniz = require("obniz");
 var obniz = new Obniz("Obniz_ID");  // Obniz_ID は自分の obniz ID
